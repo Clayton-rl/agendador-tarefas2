7c61beb7d0345f0829e52d54f8b94b9b5514aa29
Clayton <claytonprogrammer@gmail.com>
2026-04-27 11:47:08 -0300

    commit inicial com esqueleto

---
+++ src/main/java/com/javanauta/agendadortarefas2/AgendadorTarefas2Application.java
@@ -0,0 +1,13 @@
+package com.javanauta.agendadortarefas2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorTarefas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorTarefas2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanauta/agendadortarefas2/AgendadorTarefas2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.agendadortarefas2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefas2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
